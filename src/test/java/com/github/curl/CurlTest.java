07001b4d43a1243376894dbdea9b6cbc46a29369
pramodk99 <pramodkhalkar7@gmail.com>
2022-02-06 17:43:58 +0100

    release v0.0.2

--- src/test/java/com/github/curl/CurlTest.java
+++ src/test/java/com/github/curl/CurlTest.java
@@ -17,7 +17,6 @@ public class CurlTest {
     @Test
     public void failedTest() {
         RemoteCalls remoteCalls = CurlBuilder.build(RemoteCalls.class);
-
         remoteCalls.call_one(new CurlCallBack() {
             @Override
             public void onSuccess(int responseCode, String successResponse) {
